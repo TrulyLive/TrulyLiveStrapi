c3d7eb1e4ca0c74eb985ed15cf224df3e2254ed4
carlmeek <62119209+carlmeek@users.noreply.github.com>
2022-06-28 22:43:21 +0100

    Revert "Merge pull request #1 from TrulyLive/main"
    This reverts commit 71674ecf41e7474ab8fd087c8ad0e7f2f1b125e6, reversing
    changes made to 7de1529cd1ccf57ae115078cf910426f09432a87.

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,7 +5,7 @@ import Image from 'next/image'
 import { useRouter } from 'next/router'
 import cookie from 'js-cookie'
 
-function Navbar({ navData }) {
+function Navbar({ navData, className }) {
   const classStr =
     'absolute left-0 top-full pl-20 py-8 w-full space-y-6 bg-black lg:py-0 lg:pl-0 lg:space-y-0 lg:w-max lg:static lg:flex gap-6 justify-between items-center text-scGray6'
   const [open, setOpen] = useState(false)
@@ -25,7 +25,7 @@ function Navbar({ navData }) {
   const router = useRouter()
 
   return (
-    <nav className="bg-pureBlack text-pureWhite fixed top-0 w-full z-50">
+    <nav className={`bg-pureBlack text-pureWhite fixed top-0 w-full z-50 ${className}`}>
       <div className="container px-8 w-11/12 max-w-6xl mx-auto lg:px-0 flex justify-between items-center">
         <Link href="/">
           <a>
@@ -38,7 +38,7 @@ function Navbar({ navData }) {
           {navData.navlink.map((link) => (
             <li key={link.id}>
               <Link href={`/#${formatLink(link.navlink)}`}>
-                <a onClick={() => setOpen(false)} className="text-gray-300 hover:text-pmRed1 font-light">
+                <a onClick={() => setOpen(false)} className="text-scGray7 hover:text-pmRed1 font-light">
                   {link.navlink}
                 </a>
               </Link>
@@ -46,7 +46,7 @@ function Navbar({ navData }) {
           ))}
           <li>
             <Link href="/events">
-              <a onClick={() => setOpen(false)} className="text-gray-300 hover:text-pmRed1 font-light">
+              <a onClick={() => setOpen(false)} className="text-scGray7 hover:text-pmRed1 font-light">
                 Events
               </a>
             </Link>
@@ -55,7 +55,7 @@ function Navbar({ navData }) {
             <>
               <li>
                 <Link href="/login">
-                  <a onClick={() => setOpen(false)} className="text-gray-300 hover:text-pmRed1 font-light">
+                  <a onClick={() => setOpen(false)} className="text-scGray7 hover:text-pmRed1 font-light">
                     Login
                   </a>
                 </Link>
@@ -65,16 +65,21 @@ function Navbar({ navData }) {
             <>
               <li>
                 <Link href="/account">
-                  <a className="text-gray-300 hover:text-pmRed1 font-light cursor-pointer">Account</a>
+                  <li className="text-scGray7 hover:text-pmRed1 font-light cursor-pointer">Account</li>
                 </Link>
               </li>
               <li>
                 <Link href="/watching">
-                  <a className="text-gray-300 hover:text-pmRed1 font-light cursor-pointer">Watching</a>
+                  <li className="text-scGray7 hover:text-pmRed1 font-light cursor-pointer">Watching</li>
+                </Link>
+              </li>
+              <li>
+                <Link href="/video">
+                  <li className="text-scGray7 hover:text-pmRed1 font-light cursor-pointer">Watch a show</li>
                 </Link>
               </li>
               <li
-                className="text-gray-300 hover:text-pmRed1 font-light cursor-pointer"
+                className="text-scGray7 hover:text-pmRed1 font-light cursor-pointer"
                 onClick={() => {
                   cookie.remove('token')
                   router.push('/')
--- pages/_app.js
+++ pages/_app.js
@@ -2,16 +2,13 @@ import '../styles/globals.css'
 import { ToastContainer, toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import NextNprogress from 'nextjs-progressbar'
-import AuthState from '../context/AuthState'
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <NextNprogress color="#ee1f26" height={2} />
-      <AuthState>
-        <Component {...pageProps} />
-        <ToastContainer />
-      </AuthState>
+      <Component {...pageProps} />
+      <ToastContainer />
     </>
   )
 }
