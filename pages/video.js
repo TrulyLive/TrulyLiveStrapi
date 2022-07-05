5517be5193754ed6051b99ff3ee9aa9ee20b0a5e
carlmeek <62119209+carlmeek@users.noreply.github.com>
2022-07-05 08:57:26 +0100

    Update video.js

--- pages/video.js
+++ pages/video.js
@@ -211,7 +211,7 @@ export default function Home({ navData, footerData, videoData, profileData, toke
         </div>
       </div>
       <Footer footerData={footerData} />
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/js/all.min.js"></script>
+      <script async src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/js/all.min.js"></script>
     </>
   )
 }
